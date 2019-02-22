e2e3748ffdcd3a05a1b364f130cb9e012b24c2e1
daniel stupar <danielstupar95@gmail.com>
2019-02-22 09:07:12 +0100

    made a animation and script for ranged attacker #2

---
+++ Prosjektarbeid-2/entities/enemies/rigthball.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+const SPEED = 200
+var velocity = Vector2()
+
+func _ready():
+	
+	pass
+
+func _physics_process(delta):
+	velocity.x = SPEED * delta
+	translate(velocity)
+
+
+func _on_VisibilityNotifier2D_viewport_exited(viewport):
+	queue_free()
