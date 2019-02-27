3b46e78cb15af7bffc896215cc4052d6cc366156
Håkon <hakon0809@hotmail.com>
2019-02-27 15:50:25 +0100

    #29 #30 Made smoother attack animation/button pressing code

--- Prosjektarbeid-2/entities/player/KinematicBody2D.gd
+++ Prosjektarbeid-2/entities/player/KinematicBody2D.gd
@@ -14,19 +14,22 @@ func _physics_process(delta):
 	motion.y += GRAVITY
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
-		$Sprite.flip_h = false
-		$Sprite.play("Run")
+		if !$Sprite.get_animation() == "Melee2":
+			motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
+			$Sprite.flip_h = false
+			$Sprite.play("Run")
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
-		$Sprite.flip_h = true
-		$Sprite.play("Run")
+		if !$Sprite.get_animation() == "Melee2":
+			motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
+			$Sprite.flip_h = true
+			$Sprite.play("Run")
 	elif Input.is_action_pressed("ui_attack"):
 		$Sprite.play("Melee2")
 	else:
-		$Sprite.play("Idle")
-		friction = true
-		motion.x = lerp(motion.x, 0, 0.2)
+		if !$Sprite.get_animation() == "Melee2"|| $Sprite.get_frame() == 5:
+			$Sprite.play("Idle")
+			friction = true
+			motion.x = lerp(motion.x, 0, 0.2)
 		
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
