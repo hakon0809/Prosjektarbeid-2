89c1bb90c7d38f8014409777b09bbd7f60a505c4
terjesv <42997570+terjesv@users.noreply.github.com>
2019-03-01 05:43:12 +0100

    #13 added annoying popup

--- Prosjektarbeid-2/menus/choices/PrivacySettings.gd
+++ Prosjektarbeid-2/menus/choices/PrivacySettings.gd
@@ -1,13 +1,15 @@
-extends Node
+extends Container
 
 onready var content = preload("res://menus/choices/MenuText.gd")
 onready var setting = $PanelContainer/VBoxContainer/MarginContainer/VBoxContainer/HBoxContainer/Setting
 onready var button = $PanelContainer/VBoxContainer/MarginContainer/VBoxContainer/HBoxContainer/Button
+onready var popup = $PanelContainer/VBoxContainer/MarginContainer/PopupPanel
 var active
 var level
 var parent
 
 func _ready():
+	popup.hide()
 	setting.set_text(content.SETTING[level-1])
 	if active:
 		button.pressed = true
@@ -15,11 +17,22 @@ func _ready():
 		button.pressed = false
 
 func _on_SaveButton_pressed():
+	if active:
+		parent.save_setting(active)
+	else:
+		popup.show()
+	
+func _on_Button_toggled(button_pressed):
+	if active:
+		active = false
+	else:
+		active = true
+
+func _on_YesButton_pressed():
+	popup.hide()
 	parent.save_setting(active)
 
-func _on_Button_button_up():
-	active = false
 
+func _on_NoButton_pressed():
+	popup.hide()
 
-func _on_Button_button_down():
-	active = true
