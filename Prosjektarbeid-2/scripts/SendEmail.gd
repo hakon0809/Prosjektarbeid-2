1844e69915dfb1dbd1465d3b58a4928e7c478932
terjesv <42997570+terjesv@users.noreply.github.com>
2019-03-11 14:44:13 +0100

    made e-mail adress

--- Prosjektarbeid-2/scripts/SendEmail.gd
+++ Prosjektarbeid-2/scripts/SendEmail.gd
@@ -1,5 +1,5 @@
 extends Node
 
 func send_mail(results):
-	#TODO fix format
-	OS.shell_open("mailto:insert@email.here?subject=Results&body="+results)
+	#TODO fix format -> 1st/2nd/3rd choice, changing settings, ballon game
+	OS.shell_open("mailto:daretoshare.results@gmail.com?subject=Results&body="+results)
