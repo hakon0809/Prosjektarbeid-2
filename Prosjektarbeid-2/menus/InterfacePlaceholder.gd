4f04ccc2cc10141fa95f42ebe2cf6cf0eb05d95f
terjesv <42997570+terjesv@users.noreply.github.com>
2019-03-19 18:27:36 +0100

    removed bothersome canvaslayer

--- Prosjektarbeid-2/menus/InterfacePlaceholder.gd
+++ Prosjektarbeid-2/menus/InterfacePlaceholder.gd
@@ -1,18 +1,26 @@
 extends CanvasLayer
 
-onready var pause_menu = $PauseMenu/MarginContainer
+onready var pause_menu = $PauseMenu
+var paused
 
 func _ready():
 	pause_menu.hide()
-	
+	paused = false
 
 func _on_PauseButton_pressed():
-	get_tree().paused = true
-	pause_menu.show()
+	if paused:
+		get_tree().paused = false
+		paused = false
+		pause_menu.hide()
+	else:
+		get_tree().paused = true
+		paused = true
+		pause_menu.show()
 
 
 func _on_ResumeButton_pressed():
 	get_tree().paused = false
+	paused = false
 	pause_menu.hide()
 
 
