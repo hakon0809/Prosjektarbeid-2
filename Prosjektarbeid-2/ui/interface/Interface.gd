3e37e29cd065f85cf18a2221a384822462aebf3c
terjesv <42997570+terjesv@users.noreply.github.com>
2019-03-20 14:32:29 +0100

    #72 merged interface code

--- Prosjektarbeid-2/ui/interface/Interface.gd
+++ Prosjektarbeid-2/ui/interface/Interface.gd
@@ -3,11 +3,35 @@ extends CanvasLayer
 onready var number = $Control/HBoxContainer/MarginContainer/Background/Number
 onready var bar = $Control/HBoxContainer/TextureProgress
 
+onready var pause_menu = $PauseMenu
+var paused
+
+
 func _ready():
 	var player_max_health = $"../KinematicBody2D".max_health
 	bar.max_value = player_max_health
-
+	pause_menu.hide()
+	paused = false
 
 func _on_KinematicBody2D_health_changed(player_health):
 	bar.value = player_health
 	number.text = str(player_health)
+
+func _on_PauseButton_pressed():
+	if paused:
+		get_tree().paused = false
+		paused = false
+		pause_menu.hide()
+	else:
+		get_tree().paused = true
+		paused = true
+		pause_menu.show()
+
+func _on_ResumeButton_pressed():
+	get_tree().paused = false
+	paused = false
+	pause_menu.hide()
+
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
