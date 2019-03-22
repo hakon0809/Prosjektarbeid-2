e9605304cd2e175e049fe44a556aa49eb54db5f6
Sigurd Aasbrenn <sigurd.aasbrenn@gmail.com>
2019-03-22 14:22:14 +0100

    #64 Fixed the next level area and other minor fixes

--- Prosjektarbeid-2/levels/prison/NextLevel.gd
+++ Prosjektarbeid-2/levels/prison/NextLevel.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-func _on_Area2D_body_entered(body):
+
+func _on_NextLevel_body_entered(body):
 	print("Master has given Doby a sock!")
 	#get_tree().change_scene("res://levels/.....")
-
