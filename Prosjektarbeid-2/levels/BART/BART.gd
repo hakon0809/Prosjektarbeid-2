dfc12d9909c538308de8614fd790c649cdbeadc5
terjesv <42997570+terjesv@users.noreply.github.com>
2019-03-25 18:25:30 +0100

    #139 made basic layout and functionality for the balloon game

--- Prosjektarbeid-2/levels/BART/BART.gd
+++ Prosjektarbeid-2/levels/BART/BART.gd
@@ -1,27 +1,47 @@
 extends Node
 
-var rounds = 5
-var max_pumps = 10
+var rounds = 10
+var max_pumps
 var score = 0
+var total_score = 0
+var rounds_won = 0
+var burst_score = 0
+var bursts = 0
 
 func _ready():
 	randomize()
 	new_round()
 	
 func new_round():
-	max_pumps = 10
+	max_pumps = 32
 	score = 0
+	$MarginContainer/HBoxContainer/ScoreLabel.text = str(score)
+	$BalloonSprite.scale = Vector2(0, 0)
 	
 func _on_PumpButton_pressed():
-	if max_pumps == explode(max_pumps):
+	if explode(max_pumps+1):
 		rounds -= 1
-		new_round()
+		burst_score += score
+		bursts += 1
+		var average = burst_score / bursts
+		print(str(average))
+		if rounds > 0:
+			new_round()
 	else:
+		$BalloonSprite.scale += Vector2(0.2, 0.2)
 		score += 1
+		$MarginContainer/HBoxContainer/ScoreLabel.text = str(score)
 		max_pumps -= 1
 
 func _on_CollectButton_pressed():
-	new_round()
+	total_score += score
+	$MarginContainer/HBoxContainer/TotalScoreLabel.text = str(total_score)
+	rounds -= 1
+	rounds_won += 1
+	if rounds > 0:
+		new_round()
+	else:
+		pass
 	
 func explode(number):
-	return randi()%number+1
+	return number == randi()%number+1
