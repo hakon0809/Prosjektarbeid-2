85f01bd1d28c4ccf5475d3d6a05bb7a280c12e6d
Leif Ulvund <leif.ulvund@gmail.com>
2019-04-03 15:26:04 +0200

    #135 Added background and left camera limit

--- Prosjektarbeid-2/levels/fighting_level_3/Fighting_level_3.gd
+++ Prosjektarbeid-2/levels/fighting_level_3/Fighting_level_3.gd
@@ -3,6 +3,7 @@ extends Node2D
 var max_depth = 1200
 
 func _ready():
+	$Player/KinematicBody2D/Camera2D.limit_left = -80
 	$Player/KinematicBody2D/Camera2D.limit_right = 2200
 
 func _on_Area2D_body_entered(body):
