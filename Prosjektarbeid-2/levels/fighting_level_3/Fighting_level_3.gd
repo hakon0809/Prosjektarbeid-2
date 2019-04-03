75fa0bd6f6df9fa2c548da25fb74c62788685f70
Leif Ulvund <leif.ulvund@gmail.com>
2019-04-03 13:15:35 +0200

    #135 Added end of level teleport and changed character placement

--- Prosjektarbeid-2/levels/fighting_level_3/Fighting_level_3.gd
+++ Prosjektarbeid-2/levels/fighting_level_3/Fighting_level_3.gd
@@ -4,3 +4,7 @@ var max_depth = 1200
 
 func _ready():
 	$Player/KinematicBody2D/Camera2D.limit_right = 2200
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("character"):
+		#Change scene
