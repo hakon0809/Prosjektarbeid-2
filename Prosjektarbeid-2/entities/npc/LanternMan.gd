a7b7959c8b5f10e605b80f001e78eaeeb31f8b1b
Sigurd Aasbrenn <sigurd.aasbrenn@gmail.com>
2019-04-03 16:00:36 +0200

    #57 Added interaction functionality to player and lanternMan as an example

--- Prosjektarbeid-2/entities/npc/LanternMan.gd
+++ Prosjektarbeid-2/entities/npc/LanternMan.gd
@@ -1,22 +1,57 @@
 extends Node2D
 
+var player
+var dialog_text
+var button
+var dialog
+var interface
 
+var dialogArray = [
+	"Det var",
+	"en lang",
+	"apekatt"
+	]
+var dialogIndex
 
 func _ready():
+	player = get_node("../../Player")
+	dialog_text = $PopupDialog/PopupDialog/PanelContainer/MarginContainer/HBoxContainer/RichTextLabel
+	dialog = $PopupDialog/PopupDialog
+	button = $PopupDialog/PopupDialog/PanelContainer/MarginContainer/HBoxContainer/NextButton
+	interface = get_node("../../Player/Interface/Control")
+	
 	$speech_bubble.hide()
-	pass
+	dialog.hide()
 	
-	
+	dialogIndex = 0
 
 func _on_Area2D_body_entered(body):
-	if body.is_in_group("character"):
+	if body.is_in_group("character") && dialogIndex < dialogArray.size():
 		$speech_bubble.show()
-		
-	pass
+		player.setDialogueSource(self.get_path())
 	
 
 
 func _on_Area2D_body_exited(body):
 	if body.is_in_group("character"):
 		$speech_bubble.hide()
-	pass
+		player.setDialogueSource(null)
+
+
+func converse():
+	dialog.show()
+	interface.hide()
+	$speech_bubble.hide()
+	if dialogIndex >= dialogArray.size():
+		dialog_text.set_text("Get out of my face, scum.")
+	else:
+		dialog_text.set_text(dialogArray[dialogIndex])
+
+func _on_NextButton_pressed():
+	dialogIndex += 1
+	if dialogIndex >= dialogArray.size():
+		dialog.hide()
+		interface.show()
+	else:
+		dialog_text.set_text(dialogArray[dialogIndex])
+	
