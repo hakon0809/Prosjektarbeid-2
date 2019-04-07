31ede3f1ca8c0880389a0939afaafb1ca9c11ae3
Leif Ulvund <leif.ulvund@gmail.com>
2019-04-07 13:51:43 +0200

    #163 Created a viable map for skull level 2

--- Prosjektarbeid-2/levels/skull_levels/Skull_level_2.gd
+++ Prosjektarbeid-2/levels/skull_levels/Skull_level_2.gd
@@ -2,6 +2,11 @@ extends Node2D
 
 func _ready():
 	$Player/KinematicBody2D/Camera2D.limit_left = 0
-	$Player/KinematicBody2D/Camera2D.limit_right = 1000
+	$Player/KinematicBody2D/Camera2D.limit_right = 650
+	$Player/KinematicBody2D/Camera2D.limit_bottom = 250
 	$Skull.scale = Vector2(-1, 1)
 	$Skull.level = 2
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("character") && $Skull.encountered:
+		print("Dra meg baklengs gjennom spaken, Kronk")
