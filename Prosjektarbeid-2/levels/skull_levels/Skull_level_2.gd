13d67be4cb33ab84217364aab3f05de9da969692
Leif Ulvund <leif.ulvund@gmail.com>
2019-04-07 14:25:30 +0200

    163 Made text correspond to levels, and added background

--- Prosjektarbeid-2/levels/skull_levels/Skull_level_2.gd
+++ Prosjektarbeid-2/levels/skull_levels/Skull_level_2.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
+var level = 2
+
 func _ready():
 	$Player/KinematicBody2D/Camera2D.limit_left = 0
 	$Player/KinematicBody2D/Camera2D.limit_right = 650
 	$Player/KinematicBody2D/Camera2D.limit_bottom = 250
 	$Skull.scale = Vector2(-1, 1)
-	$Skull.level = 2
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("character") && $Skull.encountered:
