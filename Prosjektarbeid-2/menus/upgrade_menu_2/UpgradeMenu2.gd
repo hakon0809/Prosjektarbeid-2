b5519ef370faa5aed818e0e201e2613da52d428c
hans andreas <hansa.hestad@gmail.com>
2019-04-10 14:42:59 +0200

    #180 upgrade menu 1

--- Prosjektarbeid-2/menus/upgrade_menu_2/UpgradeMenu2.gd
+++ Prosjektarbeid-2/menus/upgrade_menu_2/UpgradeMenu2.gd
@@ -1,7 +1,10 @@
 extends Container
 
-onready var scene = $MarginContainer/PanelContainer/VBoxContainer/SceneContainer
+onready var popup = $MarginContainer/PanelContainer/Popup
+onready var scenes = $MarginContainer/PanelContainer/VBoxContainer/SceneContainer
 
 func _ready():
-	var scene_1 = load("res://menus/upgrade_menu_2/Scene1.tscn").instance()
-	scene.add_child(scene_1)
+	popup.hide()
+
+func _on_ExitButton_pressed():
+	popup.show()
