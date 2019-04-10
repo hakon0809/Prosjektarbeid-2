bc38812fd1e7dec36c1200992050c1f8b3625d2e
terjesv <42997570+terjesv@users.noreply.github.com>
2019-04-10 21:50:16 +0200

    #182 finished upgrade menu 3

---
+++ Prosjektarbeid-2/menus/upgrade_menu_3/UpgradeMenu3.gd
@@ -0,0 +1,78 @@
+extends Container
+
+onready var popup = $MarginContainer/PanelContainer/Popup
+onready var scene1 = $MarginContainer/PanelContainer/VBoxContainer/ContentContainer/Scene1
+onready var scene2 = $MarginContainer/PanelContainer/VBoxContainer/ContentContainer/Scene2
+onready var scene3 = $MarginContainer/PanelContainer/VBoxContainer/ContentContainer/Scene3
+onready var scene4 = $MarginContainer/PanelContainer/VBoxContainer/ContentContainer/Scene4
+onready var scene5 = $MarginContainer/PanelContainer/VBoxContainer/ContentContainer/Scene5
+
+var choices = [true, true, true]
+var current_setting = 0
+
+onready var toggle = $MarginContainer/PanelContainer/VBoxContainer/ContentContainer/Setting/HBoxContainer/TextureButton
+
+func _ready():
+	popup.hide()
+	scene2.hide()
+	scene3.hide()
+	scene4.hide()
+	scene5.hide()
+
+func _on_ExitButton_pressed():
+	popup.scene = self
+	popup.show()
+	
+func save_changes():
+	#save changes
+	self.queue_free()
+
+func _on_Scene1Continue_pressed():
+	scene1.hide()
+	scene2.set_text(current_setting)
+	scene2.show()
+
+func _on_AgreeButton_pressed():
+	if current_setting < 2:
+		current_setting += 1
+		scene2.set_text(current_setting)
+	else:
+		scene2.hide()
+		#android popup
+		scene5.show()
+
+func _on_InfoButton_pressed():
+	scene2.hide()
+	scene3.set_text(current_setting)
+	scene3.show()
+
+func _on_BackButton_pressed():
+	scene3.hide()
+	scene2.show()
+
+
+func _on_ManageButton_pressed():
+	scene3.hide()
+	scene4.set_text(current_setting, choices[current_setting])
+	scene4.show()
+
+
+func _on_SaveButton_pressed():
+	if $MarginContainer/PanelContainer/VBoxContainer/ContentContainer/Scene4/HBoxContainer/TextureButton.pressed:
+		choices[current_setting] = true
+	else:
+		choices[current_setting] = false
+	if current_setting < 2:
+		current_setting += 1
+		scene2.set_text(current_setting)
+		scene4.hide()
+		scene2.show()
+	else:
+		scene4.hide()
+		#android popup
+		scene5.show()
+	
+
+
+func _on_FinishButton_pressed():
+	self.queue_free()
