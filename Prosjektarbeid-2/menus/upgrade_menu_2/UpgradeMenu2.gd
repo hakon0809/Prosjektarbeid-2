20d3b8f5c5f28e594c1dd31c72ee939929e46040
terjesv <42997570+terjesv@users.noreply.github.com>
2019-04-10 13:44:22 +0200

    more same

--- Prosjektarbeid-2/menus/upgrade_menu_2/UpgradeMenu2.gd
+++ Prosjektarbeid-2/menus/upgrade_menu_2/UpgradeMenu2.gd
@@ -1,2 +1,7 @@
 extends Container
 
+onready var scene = $MarginContainer/PanelContainer/VBoxContainer/SceneContainer
+
+func _ready():
+	var scene_1 = load("res://menus/upgrade_menu_2/Scene1.tscn").instance()
+	scene.add_child(scene_1)
