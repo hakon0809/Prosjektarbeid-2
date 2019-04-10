095b004ae70b90f3e642fefd82c79a35a4959d03
terjesv <42997570+terjesv@users.noreply.github.com>
2019-04-10 16:35:23 +0200

    #181 readded upgrade2 folder

--- Prosjektarbeid-2/menus/upgrade_menu_2/Popup.gd
+++ Prosjektarbeid-2/menus/upgrade_menu_2/Popup.gd
@@ -6,27 +6,18 @@ onready var back = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/B
 var page = 1
 var scene
 
-func _ready():
-	text.text = "Are you sure you want to refuse the upgrade?"
-	confirm.text = "Yes"
-	back.text = "No"
-	
 func _on_BackButton_pressed():
 	self.hide()
 
 func _on_ConfirmButton_pressed():
 	if page == 1:
 		page += 1
-		text.text = "You likely will die without this upgrade"
-		confirm.text = "I am fine with dying"
-		back.text = "Take me back"
+		text.text = "This will reduce your chances of reaching the border"
+		confirm.text = "I DON'T CARE"
 	elif page == 2:
 		page += 1
 		text.text = "Absolutely sure?"
-		confirm.text = "Yes"
-		back.text = "No"
-	elif page == 3:
-		#Go back to game
-		pass
+		confirm.text = "YES"
 	else:
-		scene.save_changes(false)
+		scene.choices = [false, false, false]
+		scene.save_changes()
