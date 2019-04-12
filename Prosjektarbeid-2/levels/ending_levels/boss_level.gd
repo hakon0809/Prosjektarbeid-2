05a41b473efb80d4f9393dd15c82bb84b78b946a
Håkon <hakon0809@hotmail.com>
2019-04-12 10:04:25 +0200

    #192 added restrictions to boss area and progress area for when you beat the boss

--- Prosjektarbeid-2/levels/ending_levels/boss_level.gd
+++ Prosjektarbeid-2/levels/ending_levels/boss_level.gd
@@ -2,7 +2,18 @@ extends Node2D
 
 
 
-func _on_Area2D_body_entered(body):
+func _on_EnterBoss_body_entered(body):
+	if body.is_in_group("character"):
+		print("wii")
+		$Platforms/Restrict.set_position(Vector2(0,-150))
+		#when you have beat the boss, use this to allow progress
+		#$Platforms/Allow.set_position(Vector2(0,100))
+		$Player/KinematicBody2D/Camera2D.limit_left = 80
+		#change scene
+		pass
+
+
+func _on_NextArea_body_entered(body):
 	if body.is_in_group("character"):
 		print("wiiiii")
 		#change scene
