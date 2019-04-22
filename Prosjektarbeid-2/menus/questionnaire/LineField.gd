cb768cf580ed3e204cfca4953fcc622e5f04e881
Arthur Evensen <arthur@evensen.space>
2019-04-22 17:17:06 +0200

    Submit Button skeleton up and working.

--- Prosjektarbeid-2/menus/questionnaire/LineField.gd
+++ Prosjektarbeid-2/menus/questionnaire/LineField.gd
@@ -7,7 +7,3 @@ extends VBoxContainer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
-
-func get_bottom():
-	var bottom = self.get_children().back()
-	return bottom.rect_position.y + bottom.rect_size.y + self.position.y
--- Prosjektarbeid-2/menus/questionnaire/Questionnaire.gd
+++ Prosjektarbeid-2/menus/questionnaire/Questionnaire.gd
@@ -7,36 +7,31 @@ var questions = [	{	"type":"line",
 					{	"type":"line",
 						"question":"Will this show up well?"}]
 					
-
-var lineNode = load("res://menus/questionnaire/LineField.tscn")
+var rel = "res://menus/questionnaire/"
+var line_node = load("res://menus/questionnaire/LineField.tscn")
+var submit_button = load(rel + "SubmitButton.tscn")
 
 var voffset = 30 #vertical offset for ass. purposes
-var vdist = 15 #vertical distance between questions
 
 var qnodes = [] #Seems procedurally generated nodes aren't so easy to access with get_tree().get_root().find_child stuff
 var question_function = {} #Maps question types to their generating functions
 
-func set_question_position(qinstance, num):
-	var prev_y = voffset
-	if num > 0:
-		prev_y = qnodes[num-1].get_bottom()
-
-	qinstance.position.y = prev_y + vdist
-
 func generate_line(question, num):
-	var qinstance = lineNode.instance()
+	var qinstance = line_node.instance()
 	var qdict = {}
 	for c in qinstance.get_children():
 		qdict[c.get_class()] = c
 	
-	#set_question_position(qinstance, num)
 	
 	qdict.Label.text = question["question"]
 	
 	add_child(qinstance)
 	qnodes.append(qinstance)
 	
-
+func generate_submit():
+	var sbinstance = submit_button.instance()
+	add_child(sbinstance)
+	sbinstance.connect("pressed", self, "_on_SubmitButton_pressed")
 
 func _ready():
 	question_function = {
@@ -50,9 +45,11 @@ func _ready():
 	
 	for i in range(questions.size()):
 		question_function[questions[i]["type"]].call_func(questions[i], i)
-		
+	generate_submit()
 		
 
+func _on_SubmitButton_pressed():
+	print("Yeehaw my yoodle.")
 
 func leaving():
 	VisualServer.set_default_clear_color("4d4d4d") #Default colour per project settings
