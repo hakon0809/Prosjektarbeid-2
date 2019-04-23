9041452968d53fdec1d8d3b09c93c6bd8b5537a8
Håkon <hakon0809@hotmail.com>
2019-04-23 16:12:00 +0200

    #192 connected boss scene to ending scene

--- Prosjektarbeid-2/levels/ending_levels/boss_level.gd
+++ Prosjektarbeid-2/levels/ending_levels/boss_level.gd
@@ -9,12 +9,8 @@ func _on_EnterBoss_body_entered(body):
 		#when you have beat the boss, use this to allow progress
 		#$Platforms/Allow.set_position(Vector2(0,100))
 		$Player/KinematicBody2D/Camera2D.limit_left = 80
-		#change scene
-		pass
 
 
 func _on_NextArea_body_entered(body):
 	if body.is_in_group("character"):
-		print("wiiiii")
-		#change scene
-		pass
+		get_tree().change_scene("res://levels/ending_levels/ending_level.tscn")
