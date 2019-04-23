78fd9f44b1fefaa10b3329c666b34f1036be1ba7
Arthur Evensen <arthur@evensen.space>
2019-04-23 11:51:32 +0200

    Answer gathering system in place.

--- Prosjektarbeid-2/menus/questionnaire/LineField.gd
+++ Prosjektarbeid-2/menus/questionnaire/LineField.gd
@@ -7,3 +7,10 @@ extends VBoxContainer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
+
+func get_answer():
+	var question = self.find_node("Label").text
+	var answer = self.find_node("LineEdit").text
+	if not answer:
+		return null
+	return [question, answer]
--- Prosjektarbeid-2/menus/questionnaire/Questionnaire.gd
+++ Prosjektarbeid-2/menus/questionnaire/Questionnaire.gd
@@ -1,20 +1,26 @@
 extends VBoxContainer
 
-var org_vp_sz #tracker for original viewport size, might not be needed
+##var org_vp_sz #tracker for original viewport size, might not be needed after all
 
-var questions = [	{	"type":"line",
+enum {LINE}
+
+var questions = [	{	"type":LINE,
 						"question":"Do you like this question?"},
-					{	"type":"line",
+					{	"type":LINE,
 						"question":"Will this show up well?"}]
-					
+
 var rel = "res://menus/questionnaire/"
-var line_node = load("res://menus/questionnaire/LineField.tscn")
+var line_node = load(rel + "LineField.tscn")
 var submit_button = load(rel + "SubmitButton.tscn")
 
-var voffset = 30 #vertical offset for ass. purposes
+##var voffset = 30 #vertical offset for ass. purposes
 
-var qnodes = [] #Seems procedurally generated nodes aren't so easy to access with get_tree().get_root().find_child stuff
+var qnodes = [] #Seems procedurally generated nodes aren't so easy to access with get_tree().get_root().find_child stuff? Besides, heavy op.
 var question_function = {} #Maps question types to their generating functions
+
+func add_question(qinstance):
+	add_child(qinstance)
+	qnodes.append(qinstance)
 
 func generate_line(question, num):
 	var qinstance = line_node.instance()
@@ -25,20 +31,23 @@ func generate_line(question, num):
 	
 	qdict.Label.text = question["question"]
 	
-	add_child(qinstance)
-	qnodes.append(qinstance)
-	
+	add_question(qinstance)
+
 func generate_submit():
 	var sbinstance = submit_button.instance()
 	add_child(sbinstance)
 	sbinstance.connect("pressed", self, "_on_SubmitButton_pressed")
 
+func invalid_answer(qnode):
+	"Extend to handle stuff like marking an unanswered question, showing a pop-up, wutevs."
+	return null
+
 func _ready():
 	question_function = {
-		"line": funcref(self, "generate_line")
+		LINE: funcref(self, "generate_line")
 	}
 	
-	org_vp_sz = get_viewport().size
+	##org_vp_sz = get_viewport().size
 	OS.set_screen_orientation(OS.SCREEN_ORIENTATION_PORTRAIT)
 	
 	VisualServer.set_default_clear_color("fffdd0")
@@ -47,10 +56,18 @@ func _ready():
 		question_function[questions[i]["type"]].call_func(questions[i], i)
 	generate_submit()
 		
-
 func _on_SubmitButton_pressed():
+	var answers = []
 	print("Yeehaw my yoodle.")
+	for q in qnodes: #build the answers list or exit if invalid answer
+		var answer = q.get_answer()
+		answers.append(answer)
+		if answer == null:
+			return invalid_answer(q)
+	finish_questionnaire(answers)
 
-func leaving():
+func finish_questionnaire(answers):
+	#TODO: Save answers in data gathering system
+	print(answers)
 	VisualServer.set_default_clear_color("4d4d4d") #Default colour per project settings
 	OS.set_screen_orientation(OS.SCREEN_ORIENTATION_LANDSCAPE)
