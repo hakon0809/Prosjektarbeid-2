170746d89134ab5d6df62e534fb89641250cbeca
Leif Ulvund <leif.ulvund@gmail.com>
2019-04-24 13:12:26 +0200

    Made sha256_text() work

--- Prosjektarbeid-2/menus/Username.gd
+++ Prosjektarbeid-2/menus/Username.gd
@@ -4,9 +4,9 @@ var username = null
 func _on_OkButton_pressed():
 	if username != null:
 		# Checks if the game is running on android, else OS.get_unique_ID() results in an error 
-		#if Engine.has_singleton("AndroidPermissions"):
-			#var id = str(OS.get_unique_id(), username)
-			#id = id.shah256_string()
+		if Engine.has_singleton("AndroidPermissions"):
+			var id = str(OS.get_unique_id(), username)
+			Globals.player_id = id.sha256_text()
 		
 		get_tree().change_scene('res://menus/questionnaire/PreQuestionnaire.tscn')
 
